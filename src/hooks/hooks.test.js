6d2ee3d52d972dd46fc20bf5e947b2fba78be015
Birulaibe Bola de Fogo <leoschuch23@gmail.com>
2023-03-05 08:21:52 -0300

    almost done

---
+++ src/hooks/hooks.test.js
@@ -0,0 +1,17 @@
+import { renderHook } from "@testing-library/react";
+import { useState, useEffect } from "react";
+
+test('Hooks', () => {
+    const {result} = renderHook(() => {
+
+        const [nome, setNome] = useState('');
+
+        useEffect(() => {
+            setNome('Leo');
+        }, []);
+
+        return nome
+    })
+
+    expect(result.current).toBe('Leo');
+});
--- src/services/integracoes.test.js
+++ src/services/integracoes.test.js
@@ -1,4 +1,4 @@
-import { buscaTransacoes } from './transacoes';
+import { buscaTransacoes, salvaTransacao } from './transacoes';
 import api from './api.js';
 
 jest.mock('./api');
@@ -14,35 +14,66 @@ const mockTransacao = [
 ];
 
 const mockRequisicao = (retorno) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({
-                data: retorno,
-            })
-        }, 200)
-    });
-}
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno,
+      });
+    }, 200);
+  });
+};
 const mockRequisicaoErro = () => {
-    return new Promise((_, reject) => {
-        setTimeout(() => {
-            reject()
-        }, 200)
-    });
-}
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 200);
+  });
+};
+
+const mockRequisicaoPost = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        status: 201,
+      });
+    }, 200);
+  });
+};
+
+const mockRequisicaoPostErro = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 200);
+  });
+};
 
 describe('Requisições para API', () => {
-    test('Deve retornar uma lista de transações', async () => {
-        api.get.mockImplementation(() =>mockRequisicao(mockTransacao))
+  test('Deve retornar uma lista de transações', async () => {
+    api.get.mockImplementation(() => mockRequisicao(mockTransacao));
 
-        const transacoes = await buscaTransacoes();
-        expect(transacoes).toEqual(mockTransacao);
-        expect(api.get).toHaveBeenCalledWith('/transacoes')
-    })
-    test('Deve retornar uma lista vazia quando a requisição falhar', async () => {
-        api.get.mockImplementation(() =>mockRequisicaoErro())
+    const transacoes = await buscaTransacoes();
+    expect(transacoes).toEqual(mockTransacao);
+    expect(api.get).toHaveBeenCalledWith('/transacoes');
+  });
+  test('Deve retornar uma lista vazia quando a requisição falhar', async () => {
+    api.get.mockImplementation(() => mockRequisicaoErro());
 
-        const transacoes = await buscaTransacoes();
-        expect(transacoes).toEqual([]);
-        expect(api.get).toHaveBeenCalledWith('/transacoes');
-    });
+    const transacoes = await buscaTransacoes();
+    expect(transacoes).toEqual([]);
+    expect(api.get).toHaveBeenCalledWith('/transacoes');
+  });
+  test('Deve retornar um status 201 - (Created) após uma requisição POST', async () => {
+    api.post.mockImplementation(() => mockRequisicaoPost());
+    const status = await salvaTransacao(mockTransacao[0]);
+    expect(status).toBe(201);
+    expect(api.post).toHaveBeenCalledWith('/transacoes', mockTransacao[0]);
+  });
+  test('Deve retornar um saldo de 1000 quando a requisição POST falhar', async() => {
+    api.post.mockImplementation(() => mockRequisicaoPostErro());
+    const status = await salvaTransacao(mockTransacao[0])
+
+    expect(status).toBe('Erro na requisição');
+    expect(api.post).toHaveBeenCalledWith('/transacoes', mockTransacao[0]);
+  })
 });
